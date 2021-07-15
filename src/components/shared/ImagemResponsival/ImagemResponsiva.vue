0345daf02d11790ab30ba50bd3e89d65174b4311
franciscocesar63 <franciscocesar888@gmail.com>
2021-07-15 20:34:02 -0300

    VueJS - Componentes

---
+++ src/components/shared/ImagemResponsival/ImagemResponsiva.vue
@@ -0,0 +1,19 @@
+<template>
+  <img class="imagem-responsiva" :src="url" :alt="titulo" />
+</template>
+
+<script>
+export default {
+  props: ["url", "titulo"],
+};
+</script>
+
+<style scoped>
+.imagem-responsiva {
+  width: 100%;
+}
+.imagem-responsiva:hover {
+  opacity: 0.8;
+  transition-delay: 700;
+}
+</style>
