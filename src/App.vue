b28f9ac8d06c6d5927d03467ce074536cf5d5ad4
franciscocesar63 <franciscocesar888@gmail.com>
2021-07-17 11:52:15 -0300

    Parte 1 finalizada

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="corpo">
+
     <meu-menu :rotas="routes"/>
+
     <transition name="pagina">
       <router-view></router-view>
     </transition>
@@ -8,34 +10,41 @@
 </template>
 
 <script>
-import { routes } from "./routes";
-import Menu from "./components/shared/menu/Menu.vue";
+
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 
 export default {
+
   components: {
-    "meu-menu": Menu,
+    'meu-menu' : Menu
   },
+  
   data() {
+
     return {
-      routes,
-    };
-  },
-};
+
+      routes
+    }
+  }
+}
 </script>
 
 <style>
-.corpo {
-  font-family: Helvetica, sans-serif;
-  margin: 0 auto;
-  width: 96%;
-}
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 
-.pagina-enter-active,
-.pagina-leave-active {
-  transition: opacity 0.3s;
-}
-.pagina-enter,
-.pagina-leave-active {
-  opacity: 0;
-}
+ .pagina-enter, .pagina-leave-active {
+
+     opacity: 0;
+ }
+
+ .pagina-enter-active, .pagina-leave-active {
+
+     transition: opacity .4s;
+ }
+
 </style>
